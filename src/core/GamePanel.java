b6a1153a3d836dfc1ad7715de6bbea06668c8135
rostyslav-bilous <rsbil256@gmail.com>
2024-10-08 19:29:23 +0200

    Rendered a red smile

--- src/core/GamePanel.java
+++ src/core/GamePanel.java
@@ -3,21 +3,25 @@ package core;
 import java.awt.*;
 import javax.swing.*;
 
-class GamePanel extends JPanel {
+import utils.*;
 
-    int windowWidth = 720;
-    int windowHeight = 480;
+class GamePanel extends JPanel {
 
     public GamePanel() {
 
-        setPreferredSize(new Dimension(windowWidth, windowHeight));
+        setPreferredSize(new Dimension(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
         setBackground(Color.BLUE);
     }
 
-    // @Override
-    // protected void painComponent(Graphics g) {
-    //     super.paintComponent(g);
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-    // }
+        g.setColor(Color.RED);
+        g.fillRect(100, 100, Constants.WINDOW_WIDTH / 20, Constants.WINDOW_HEIGHT / 20);
+        g.fillRect(400, 100, Constants.WINDOW_WIDTH / 20, Constants.WINDOW_HEIGHT / 20);
+        g.fillRect(100, 400, Constants.WINDOW_WIDTH / 20 + 300, Constants.WINDOW_HEIGHT / 20);
+
+    }
 
 }
---
+++ src/utils/Constants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Constants {
+    
+    public static final int WINDOW_WIDTH = 1280;
+    public static final int WINDOW_HEIGHT = 720;
+
+    public static final int GAME_FPS = 60;
+}
